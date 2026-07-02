71470fc9847cf1ec1be72f474b3e281ac52312ab
Kanan <kanan2304@mail.ru>
2026-07-02 12:25:32 +0200

    added a new line

--- huntest/src/main.java
+++ huntest/src/main.java
@@ -1,5 +1,16 @@
+import com.sun.tools.javac.Main;
+
+import java.util.Objects;
+
 public class main {
     public static void main(String[] args) {
         System.out.println("Hello World");
+        main main = new main();
+        
+        System.out.println(main.returnString());
+    }
+
+    public String returnString() {
+        return "Hey there! It's Kanan, how are you?";
     }
 }
